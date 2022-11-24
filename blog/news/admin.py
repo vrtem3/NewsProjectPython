c502e9d542b8f85c64010572b3198c92c3848125
artem <vrtem.ya@yandex.ru>
2022-11-24 21:36:46 +0300

    master

--- blog/news/admin.py
+++ blog/news/admin.py
@@ -2,9 +2,21 @@ from django.contrib import admin
 from .models import Author, Category, Post, PostCategory, Comment, CategorySubscribers
 
 
-admin.site.register(Author)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'dateCreate', 'categoryType', 'rating') # выводим поля в админку
+    list_filter = ('title', 'dateCreate', 'categoryType', 'rating') # добавляем фильтры в админку
+    search_fields = ('title', 'dateCreate', 'categoryType', 'rating') # добавляем поиск по полям модели в админку
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('authorUser', 'ratingAuthor')
+    list_filter = ('authorUser', 'ratingAuthor')
+    search_fields = ('authorUser', 'ratingAuthor')
+
+
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(PostCategory)
 admin.site.register(Comment)
 admin.site.register(CategorySubscribers)
